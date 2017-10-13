534a5dbf13debc255f5b9b803ba6bff9780d26bb
Computer <vorreakboth@gmail.com>
2017-10-14 03:05:49 +0700

    news

---
+++ app/src/main/java/edu/ckcc/schoolguide/Activity/MainActivity.java
@@ -0,0 +1,35 @@
+package edu.ckcc.schoolguide.Activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import edu.ckcc.schoolguide.R;
+
+public class MainActivity extends AppCompatActivity{
+    private static int SPLASH_TIME_OUT = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
