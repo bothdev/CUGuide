b1891eebf98310ef0513f11c2ee0a065e1dc98c0
computer <vorreakboth@gmail.com>
2017-11-08 03:36:23 +0700

    update

--- app/src/main/java/edu/ckcc/schoolguide/Activity/AboutActivity.java
+++ app/src/main/java/edu/ckcc/schoolguide/Activity/AboutActivity.java
@@ -9,14 +9,27 @@ import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.style.BackgroundColorSpan;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import edu.ckcc.schoolguide.R;
  
 public class AboutActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    MapView mapView;
+    GoogleMap map;
+    private double longtitute = 11.5537852;
+    private double latitute = 104.916943;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +53,29 @@ public class AboutActivity extends AppCompatActivity implements NavigationView.O
 		//Show back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		///////////////////////////////////
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.d("ckcc" , "onMapReady");
+                map = googleMap;
+
+                addMarker();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
     }
 
     @Override
@@ -53,5 +89,17 @@ public class AboutActivity extends AppCompatActivity implements NavigationView.O
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addMarker(){
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("SchoolGuide");
+        markerOptions.snippet("Our Location");
+        LatLng latlng = new LatLng(latitute, longtitute);
+        markerOptions.position(latlng);
+        markerOptions.draggable(false);
+        map.addMarker(markerOptions);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 15);
+        map.animateCamera(cameraUpdate);
     }
 }
